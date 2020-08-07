32fd83bef71be5ec32cba2a48c1bb71a722a013e
lorenzowind <lorenzomart01@gmail.com>
2020-08-07 15:39:53 -0400

    chore(mobile browsing): initial structure configuration

---
+++ Mobile Browsing/app/src/main/java/com/example/mobilebrowsing/ExplorerActivity.java
@@ -0,0 +1,13 @@
+package com.example.mobilebrowsing;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExplorerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eplorer);
+    }
+}
